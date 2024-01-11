3ef9cd77ae9d9ab8cb0ccf38e5463d9c4f5c4c61
malkitbenning <malkitbenning@gmail.com>
2024-01-11 17:31:32 +0000

    Added menu to react starter project

---
+++ src/components/pages/Services.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+export const Services = () => {
+  return (
+    <div>
+      <div>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque, deleniti atque, quisquam blanditiis porro
+        alias numquam autem dolor ipsum provident accusantium eveniet quod! Veritatis ipsum eos asperiores impedit quae
+        modi odit unde sapiente dolores nostrum, perferendis tenetur rerum voluptatem quidem nobis velit optio.
+        Doloremque, esse? Optio ex repudiandae quasi ipsum, nesciunt sunt sequi, quod dolore maiores quidem beatae
+        tempore similique veritatis corporis a odit libero sint excepturi qui minus. Autem accusantium deleniti soluta
+        quae assumenda ad iusto expedita veniam quas in nisi sapiente, nobis aliquam dolores facilis voluptatem! Non
+        vitae, culpa ut dolor rerum repudiandae sed iusto quisquam voluptas perferendis?
+      </div>
+      <br />
+      <div>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque, deleniti atque, quisquam blanditiis porro
+        alias numquam autem dolor ipsum provident accusantium eveniet quod! Veritatis ipsum eos asperiores impedit quae
+        modi odit unde sapiente dolores nostrum, perferendis tenetur rerum voluptatem quidem nobis velit optio.
+        Doloremque, esse? Optio ex repudiandae quasi ipsum, nesciunt sunt sequi, quod dolore maiores quidem beatae
+        tempore similique veritatis corporis a odit libero sint excepturi qui minus. Autem accusantium deleniti soluta
+        quae assumenda ad iusto expedita veniam quas in nisi sapiente, nobis aliquam dolores facilis voluptatem! Non
+        vitae, culpa ut dolor rerum repudiandae sed iusto quisquam voluptas perferendis?
+      </div>
+      <br />
+      <div>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque, deleniti atque, quisquam blanditiis porro
+        alias numquam autem dolor ipsum provident accusantium eveniet quod! Veritatis ipsum eos asperiores impedit quae
+        modi odit unde sapiente dolores nostrum, perferendis tenetur rerum voluptatem quidem nobis velit optio.
+        Doloremque, esse? Optio ex repudiandae quasi ipsum, nesciunt sunt sequi, quod dolore maiores quidem beatae
+        tempore similique veritatis corporis a odit libero sint excepturi qui minus. Autem accusantium deleniti soluta
+        quae assumenda ad iusto expedita veniam quas in nisi sapiente, nobis aliquam dolores facilis voluptatem! Non
+        vitae, culpa ut dolor rerum repudiandae sed iusto quisquam voluptas perferendis?
+      </div>
+      <br />
+      <div>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque, deleniti atque, quisquam blanditiis porro
+        alias numquam autem dolor ipsum provident accusantium eveniet quod! Veritatis ipsum eos asperiores impedit quae
+        modi odit unde sapiente dolores nostrum, perferendis tenetur rerum voluptatem quidem nobis velit optio.
+        Doloremque, esse? Optio ex repudiandae quasi ipsum, nesciunt sunt sequi, quod dolore maiores quidem beatae
+        tempore similique veritatis corporis a odit libero sint excepturi qui minus. Autem accusantium deleniti soluta
+        quae assumenda ad iusto expedita veniam quas in nisi sapiente, nobis aliquam dolores facilis voluptatem! Non
+        vitae, culpa ut dolor rerum repudiandae sed iusto quisquam voluptas perferendis?
+      </div>
+      <br />
+    </div>
+  );
+};
